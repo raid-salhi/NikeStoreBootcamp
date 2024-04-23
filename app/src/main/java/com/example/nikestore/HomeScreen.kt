016f921b092a267ed1fb4a0b46fa5a01d73d9abf
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-04-23 22:22:50 +0100

    Finished starting screen ui

---
+++ app/src/main/java/com/example/nikestore/HomeScreen.kt
@@ -0,0 +1,20 @@
+package com.example.nikestore
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.nikestore.ui.theme.MyBackground
+
+@Composable
+fun HomeScreen(){
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = MyBackground
+    ){
+        Column {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/nikestore/StartScreen.kt
@@ -0,0 +1,73 @@
+package com.example.nikestore
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.nikestore.ui.theme.MyBackground
+import com.example.nikestore.ui.theme.MyBlack
+import com.example.nikestore.ui.theme.MyBlue
+
+@Preview(showBackground = true, device = "id:pixel_4")
+@Composable
+fun StartScreen(){
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = MyBlue
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.nike) ,
+                contentDescription = "Nike Logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxWidth(0.4f)
+            )
+            Text(
+                text = "NIKE",
+                fontFamily = FontFamily(listOf(Font(R.font.futura_consended_extra_black))),
+                color = Color.White,
+                fontSize = 65.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+            Button(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 50.dp, end = 50.dp, bottom = 10.dp)
+                ) {
+                Text(
+                    text = "Let’s check the store",
+                    color = MyBlack,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+}
