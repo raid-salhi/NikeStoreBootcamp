6ea8e2a2899ee4f40e0031cde018bc9d58450e82
raid-salhi <118809948+raid-salhi@users.noreply.github.com>
2024-04-24 18:36:47 +0100

    Created Shoe model and built some Home Screen

--- app/src/main/java/com/example/nikestore/HomeScreen.kt
+++ app/src/main/java/com/example/nikestore/HomeScreen.kt
@@ -1,13 +1,20 @@
 package com.example.nikestore
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -20,6 +27,7 @@ import com.example.nikestore.ui.theme.MyBlack
 @Preview(showBackground = true)
 @Composable
 fun HomeScreen(){
+    val categories = listOf("All Shoes","Outdoor","Tennis","Running")
     Surface (
         modifier = Modifier.fillMaxSize(),
         color = MyBackground
@@ -27,7 +35,7 @@ fun HomeScreen(){
         Column (
             modifier = Modifier
                 .fillMaxSize()
-                .padding(start = 20.dp, end=20.dp, top = 10.dp),
+                .padding(start = 20.dp, end = 20.dp, top = 10.dp),
         ){
             Text(
                 text = "Explore",
@@ -36,6 +44,38 @@ fun HomeScreen(){
                 fontSize = 32.sp,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
+            Text(
+                text = "Select Category",
+                color = MyBlack,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(top = 24.dp)
+            )
+            LazyRow(modifier = Modifier.padding(top = 14.dp, bottom =14.dp)) {
+                items(items=categories){
+                    CategoryCard(category=it)
+                }
+            }
+
         }
+    }
+}
+
+@Composable
+fun CategoryCard(category: String) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.White)
+            .size(width = 100.dp, height = 40.dp)
+            .padding(end = 14.dp),
+        contentAlignment = Alignment.Center
+        ){
+        Text(
+            text = category,
+            color = MyBlack,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium,
+        )
     }
 }
---
+++ app/src/main/java/com/example/nikestore/Shoe.kt
@@ -0,0 +1,8 @@
+package com.example.nikestore
+
+data class Shoe(
+    val name:String,
+    val category:String,
+    val price : Double,
+    val pic:Int,
+    )
